1abb2953bb0c78fc802a7cca86d3d9e6371a5dcb
Shubham Rai <shubham.010793@gmail.com>
2026-03-19 11:34:11 +0530

    Add Google Analytics tracking (#44)
    * Add Google Analytics tracking
    
    * Remove unrelated Sentry route wrapper
    
    * Restore Sentry route instrumentation

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,8 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+import { useEffect } from "react";
 import { AppLayout } from "@/components/layout/AppLayout";
 import { ClusterProvider } from "@/contexts/ClusterContext";
 import { ConfigProvider } from "@/contexts/ConfigContext";
@@ -18,6 +19,27 @@ const queryClient = new QueryClient();
 
 const SentryRoutes = Sentry.withSentryReactRouterV6Routing(Routes);
 
+declare global {
+  interface Window {
+    dataLayer: unknown[];
+    gtag?: (...args: unknown[]) => void;
+  }
+}
+
+const AnalyticsPageTracker = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    window.gtag?.("event", "page_view", {
+      page_path: `${location.pathname}${location.search}${location.hash}`,
+      page_title: document.title,
+      page_location: window.location.href,
+    });
+  }, [location]);
+
+  return null;
+};
+
 const App = () => (
  <Sentry.ErrorBoundary fallback={<p>An unexpected error has occurred.</p>} showDialog>
   <QueryClientProvider client={queryClient}>
@@ -27,7 +49,8 @@ const App = () => (
           <Toaster />
           <Sonner />
           <BrowserRouter>
-            <Routes>
+            <AnalyticsPageTracker />
+            <SentryRoutes>
               <Route element={<AppLayout />}>
                 <Route path="/" element={<Overview />} />
                 <Route path="/workloads" element={<Workloads />} />
@@ -36,7 +59,7 @@ const App = () => (
                 <Route path="/events" element={<Events />} />
               </Route>
               <Route path="*" element={<NotFound />} />
-            </Routes>
+            </SentryRoutes>
           </BrowserRouter>
         </ConfigProvider>
       </ClusterProvider>
